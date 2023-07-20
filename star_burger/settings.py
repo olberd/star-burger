d9ee4144135f3674f7c8dffe5da0d9dc37937d5b
Oleg <olberd@bk.ru>
2023-07-20 10:32:43 +0300

    Switch to PostgreSQL DB.

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -85,20 +85,6 @@ WSGI_APPLICATION = 'star_burger.wsgi.application'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-# DATABASES = {
-#     # 'default': dj_database_url.config(
-#     #     default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
-#     # )
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'star_burger',
-#         'USER': 'postgres',
-#         'PASSWORD': 'ol',
-#         'HOST': 'localhost',
-#         'PORT': '5433',
-#     }
-# }
-
 DATABASES = {
             'default': dj_database_url.config(default=env('POSTGRES_CONNECTION'))
 }
