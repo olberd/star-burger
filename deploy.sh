ad8f7eee8de4639855df5349a44b1b38ca32ccf2
root <root@1306500-cg33509.tw1.ru>
2023-08-12 21:45:54 +0300

    Add to bash script report to Rollbar

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
 set -e
+source .env
 
 cd /opt/star-burger/
+echo "Updating project..."
 git pull
 source env_star_burger/bin/activate
+echo "Install requirements..."
 pip install -r requirements.txt -q
-npm ci --dev --silent
-./node_modules/.bin/parcel watch bundles-src/index.js --dist-dir bundles --public-url="./"
+echo "Install frontend..."
+npm ci --dev --quiet --silent
+./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
+echo "Collect static..."
 python manage.py collectstatic --noinput
+echo "Migrate..."
 python manage.py migrate --noinput
-
+echo "Reload nginx..."
 systemctl reload nginx.service
 
+current_commit=$(git rev-parse --verify HEAD)
+report_data='{"environment": "production", "revision": '\""$current_commit"\"', "local_username": '\""$USER"\"', "status": "succeeded"}'
 
+curl \
+	-H "X-Rollbar-Access-Token: $ROLLBAR_ACCESS_TOKEN" \
+	-H "Content-Type: application/json" \
+	-X POST "https://api.rollbar.com/api/1/deploy" \
+	-d "$report_data"
+
+echo -e "\nDeploy reported to Rollbar"
 echo "Deploy completed"
 
